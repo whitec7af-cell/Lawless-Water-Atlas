2ab60fb54bd0e41e23d36a01ef87ccf51078480d
whitec7af-cell <whitec7af@gmail.com>
2026-06-15 18:25:30 -0400

    Update app.js

--- app.js
+++ app.js
@@ -2,6 +2,18 @@ const manualButton = document.getElementById("manualButton");
 const autoButton = document.getElementById("autoButton");
 const message = document.getElementById("message");
 
+const map = L.map("map").setView(
+    [40.0583, -74.4057],
+    7
+);
+
+L.tileLayer(
+    "https://tile.openstreetmap.org/{z}/{x}/{y}.png",
+    {
+        attribution: "&copy; OpenStreetMap contributors"
+    }
+).addTo(map);
+
 manualButton.addEventListener("click", () => {
     message.textContent = "Manual Locate selected.";
 });
