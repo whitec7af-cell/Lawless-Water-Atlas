2a4e904971890157ceaaec5110eaf7c486d40545
whitec7af-cell <whitec7af@gmail.com>
2026-06-15 19:23:23 -0400

    Update app.js
    add first water marker

--- app.js
+++ app.js
@@ -1,23 +1,11 @@
-const manualButton = document.getElementById("manualButton");
-const autoButton = document.getElementById("autoButton");
-const message = document.getElementById("message");
-
-const map = L.map("map").setView(
-    [40.0583, -74.4057],
-    7
-);
-
-L.tileLayer(
-    "https://tile.openstreetmap.org/{z}/{x}/{y}.png",
-    {
-        attribution: "&copy; OpenStreetMap contributors"
-    }
+const testWater = L.marker(
+    [40.903, -74.423]
 ).addTo(map);
 
-manualButton.addEventListener("click", () => {
-    message.textContent = "Manual Locate selected.";
-});
-
-autoButton.addEventListener("click", () => {
-    message.textContent = "Auto Locate selected.";
-});
+testWater.bindPopup(`
+<b>Monksville Reservoir</b>
+<br>
+County: Passaic
+<br>
+Status: Test Water
+`);
