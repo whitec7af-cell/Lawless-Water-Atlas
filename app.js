9840516e5b95b2e6461df1f91c54c9832885e958
whitec7af-cell <whitec7af@gmail.com>
2026-06-15 21:10:38 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,19 @@
+const manualButton = document.getElementById("manualButton");
+const autoButton = document.getElementById("autoButton");
+const message = document.getElementById("message");
+
+const map = L.map("map").setView(
+    [40.0583, -74.4057],
+    7
+);
+
+L.tileLayer(
+    "https://tile.openstreetmap.org/{z}/{x}/{y}.png",
+    {
+        attribution: "&copy; OpenStreetMap contributors"
+    }
+).addTo(map);
+
 const testWater = L.marker(
     [40.903, -74.423]
 ).addTo(map);
@@ -9,3 +25,11 @@ County: Passaic
 <br>
 Status: Test Water
 `);
+
+manualButton.addEventListener("click", () => {
+    message.textContent = "Manual Locate selected.";
+});
+
+autoButton.addEventListener("click", () => {
+    message.textContent = "Auto Locate selected.";
+});
